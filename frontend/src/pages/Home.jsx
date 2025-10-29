fb7571024250cc48a26f3198a2f682cc08172e60
Phạm Nguyễn Phú Thành <thanhphamnguyenphu2k5@gmail.com>
2025-10-29 11:47:48 +0700

    Build(Home): create frontend home page

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,49 @@
+import { useEffect } from "react";
+import "../styles/Home.css";
+import { Link } from "react-router-dom";
+
+function Home() {
+  useEffect(() => {
+    document.title = "Home | Flogin";
+  }, []);
+
+  return (
+    <>
+      <div className="home">
+        <div className="header">
+          <Link to={"/"} className="brand">
+            <div className="brand-logo">
+              <img src="/logo.svg" alt="logo" className="logo" />
+            </div>
+            <div className="brand-name">gin</div>
+          </Link>
+
+          <Link to={"/login"} className="signin">
+            <div className="signin-ico">
+              <img src="/ico.svg" alt="ico" className="ico" />
+            </div>
+            <div className="signin-name">Sign in</div>
+          </Link>
+        </div>
+
+        <div className="body">
+          <div className="intro">
+            <div className="intro-main-slogan">Open Your World</div>
+            <div className="intro-sub-slogan">
+              Discover stories, knowledge, and inspiration — one page at a time
+            </div>
+            <Link to={"/login"} className="intro-button">
+              <div className="intro-button-name">Get Started</div>
+            </Link>
+          </div>
+
+          <div className="picture">
+            <img src="/img.svg" alt="img" className="picture-img" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
