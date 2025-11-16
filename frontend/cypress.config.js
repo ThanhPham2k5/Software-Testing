68e4dbb54b5e615f1bebd79210f49bdc4d0245aa
JustAnother-12 <bathiennguyen024@gmail.com>
2025-11-16 22:13:51 +0700

    Create(E2E): install, setup config for Cypress

---
+++ frontend/cypress.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "cypress";
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    specPattern: [
+      "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+      "cypress/e2e/**/*.e2e.spec.js",
+    ],
+  },
+});
