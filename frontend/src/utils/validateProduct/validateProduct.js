38e67f961e4df5700e87581181e92651d3c26e8d
JustAnother-12 <bathiennguyen024@gmail.com>
2025-11-25 13:37:29 +0700

    add upload image logic

--- frontend/src/utils/validateProduct/validateProduct.js
+++ frontend/src/utils/validateProduct/validateProduct.js
@@ -5,14 +5,18 @@ export function validateProduct(product) {
     errors.name = "Product name cannot be empty";
   }
 
+  if(product.name.trim().length < 3 || product.name.trim().length > 100) {
+    errors.name = "Product name must be between 3-100 characters"
+  }
+
   const quantity = Number(product.quantity);
   if (product.quantity === "" || quantity < 0 || quantity > 99999) {
-    errors.quantity = "Product quantity must be between 0 and 99,999";
+    errors.quantity = "Product quantity must be between 0-99,999 characters";
   }
 
   const price = Number(product.price);
-  if (product.price === "" || price < 0 || price > 999999999) {
-    errors.price = "Product price must be between 0 and 999,999,999";
+  if (product.price === "" || price <= 0 || price > 999999999) {
+    errors.price = "Product price must be between 0-999,999,999 characters";
   }
 
   const desc = product.description ? product.description.trim() : "";
