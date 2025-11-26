d994806d0620972dfd76b0171d96e2649a65f846
JustAnother-12 <bathiennguyen024@gmail.com>
2025-11-26 23:54:05 +0700

    Add(backend): add SqlInjectionTest

---
+++ backend/src/test/java/com/flogin/security/SqlInjectionSecurityTest.java
@@ -0,0 +1,45 @@
+package com.flogin.security;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.flogin.dto.LoginRequestDTO;
+import com.flogin.repository.AccountRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SqlInjectionSecurityTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private AccountRepository repository;
+
+    @Test
+    @DisplayName("SQL Injection: Injection attempts must fail")
+    void testSqlInjection() throws Exception {
+        String injection = "' OR '1'='1";
+        LoginRequestDTO request = new LoginRequestDTO(injection, "any_password");
+
+        mockMvc.perform(post("/api/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.status").value(false))
+                .andExpect(jsonPath("$.token").doesNotExist());;
+    }
+}
