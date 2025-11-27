c83f904595debede7a78b75b1ad7bef72c681c47
JustAnother-12 <bathiennguyen024@gmail.com>
2025-11-27 12:44:43 +0700

    add(backend): add hashed password for seed

--- backend/src/main/java/com/flogin/SecurityConfig.java
+++ backend/src/main/java/com/flogin/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.context.annotation.Bean;
 
@@ -25,5 +26,10 @@ public class SecurityConfig {
                 )
                 .httpBasic(AbstractHttpConfigurer::disable);
         return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
